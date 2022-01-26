c870d690df5aa69808d1750596409dacfbfba548
Zhyraa <87532858+Zyhraa@users.noreply.github.com>
2022-01-26 23:35:42 +0700

    footer

--- Home/header/footer.php
+++ Home/header/footer.php
@@ -1,28 +1,34 @@
+<script src="https://kit.fontawesome.com/5681f6e595.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="stylefooter.css">
+
 <div class="footer">
     <div class="inside">
-        <div class="logo">
-            <h6>Powered by:</h6>
-            <h4>AniMov</h4>
-        </div>
+        <div class="row">
+            <div class="logo">
+                <h6>Powered by:</h6>
+                <img src="../../img/logo.png" alt="">
+            </div>
 
-        <div class="address">
-            <h6>AniMov</h6>
-            <p>Jl. Raya Siliwangi No.6, <br> RT.001/RW.004, Sepanjang Jaya, <br> Kec. Rawalumbu, Kota Bekasi.</p>
-            <div class="line"></div>
-        </div>
+            <div class="address">
+                <h6>AniMov</h6>
+                <p>Jl. Raya Siliwangi No.6, <br> RT.001/RW.004, Sepanjang Jaya, <br> Kec. Rawalumbu, Kota Bekasi.</p>
+                <div class="line"></div>
+                <span>@ 2022 AniMov All Rights Reserved </span>
+            </div>
 
-        <div class="about">
-            <h6>About Us</h6>
-            <span>Social Media</span>
-            <span>Website</span>
-            <span>0811-9-352-353</span>
-        </div>
+            <div class="about">
+                <h6>About Us</h6>
+                <p>Social Media</p>
+                <p>Website</p>
+                <p>0811-9-352-353</p>
+            </div>
 
-        <div class="sosmed">
-            <a href="twitter"><img src="" alt=""></a>
-            <a href="facebook"><img src="" alt=""></a>
-            <a href="tiktok"><img src="" alt=""></a>
-            <a href="instagram"><img src="" alt=""></a>
+            <div class="sosmed">
+                <a href="twitter"><i class="fab fa-twitter"></i></a>
+                <a href="facebook"><i class="fab fa-facebook-f"></i></a>
+                <a href="tiktok"><i class="fab fa-tiktok"></i></a>
+                <a href="instagram"><i class="fab fa-instagram"></i></a>
+            </div>
         </div>
     </div>
 </div>
