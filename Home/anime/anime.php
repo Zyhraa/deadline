ebbe12ea94d22f17395642b3aff8c33a8e459929
Zhyraa <87532858+Zyhraa@users.noreply.github.com>
2022-01-27 04:47:45 +0700

    hi

--- Home/anime/anime.php
+++ Home/anime/anime.php
@@ -1,21 +1,21 @@
-<?php
-include '../connection.php'; 
-
-$result = mysqli_query($con, "SELECT * FROM anime");
-while ($row = mysqli_fetch_array($result)) { ?>
-
-<link rel="stylesheet" type="text/css" href="animestyle.css">
-
-    <div class="whole">
-		<div class="items">
-			<a href="google.com">
-				<div class="item">	
-                    <img src="animecover/<?=$row['img']?>">
+<div class="kotakKu">
+    <?php
+    include '../../connection.php';
+    
+    $result = mysqli_query($con, "SELECT * FROM anime");
+    while ($row = mysqli_fetch_array($result)) { ?>
+    
+    <link rel="stylesheet" type="text/css" href="animestyle.css">
+        <div class="items">
+            <a href="google.com">
+                <div class="item">
+                    <img src="../animecover/<?=$row['img']?>">
                     <h1><?=$row['title']?></h1>
                 </div>
             </a>
         </div>
     <?php } ?>
-    </div>
+</div>
+
 </body>
 </html>
