a33894dc6639757adecc433c92e96853aca13ded
Zhyraa <87532858+Zyhraa@users.noreply.github.com>
2022-01-27 20:14:48 +0700

    co

---
+++ Home/merchandise/checkout.php
@@ -0,0 +1,24 @@
+<?php include '../header/navbar.php'; ?>
+<style><?php include '../header/stylenav.css'; ?></style>
+
+<link rel="stylesheet" href="checkout.css">
+
+<div class="bg">
+    <div class="black">
+        <div class="content">
+
+
+
+
+
+
+
+
+
+
+<?php include '../header/footer.php'; ?>
+<style><?php include '../header/stylefooter.css'; ?>
+.footer{
+    position: relative;
+}
+</style>
