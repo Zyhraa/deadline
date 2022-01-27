da540424502c61b378c7bc4711129f7292ab192f
Zhyraa <87532858+Zyhraa@users.noreply.github.com>
2022-01-27 12:33:35 +0700

    adwasda

--- Home/index.php
+++ Home/index.php
@@ -78,6 +78,47 @@
             </div>
         </div>
 
+        <div class="anime-catthree">
+            <div class="series-anime">
+                <div>Nostalgic Anime</div>
+                <div>See All</div>
+            </div>
+            <div class="animelist-popular">
+                <div class="contohkotak">
+                    <img src="animecover/103920.jpg" alt="">
+                </div>
+                <!--<div class="kotakKu">
+                    <?php
+                    //include '../connection.php';
+                    
+                    //$result = mysqli_query($con, "SELECT * FROM anime");
+                    //while ($row = mysqli_fetch_array($result)) { ?>
+                    
+                    <link rel="stylesheet" type="text/css" href="animestyle.css">
+                        <div class="items">
+                            <a href="google.com">
+                                <div class="item">
+                                    <img src="animecover/<?//=$row['img']?>">
+                                    <h1><?//=$row['title']?></h1>
+                                </div>
+                            </a>
+                        </div>
+                    <?php //} ?>
+                </div>-->
+            </div>
+        </div>
+
+        <div class="anime-catfour">
+            <div class="features-anime">
+                <div>AniMov Features</div>
+            </div>
+            <div class="kotakkotak">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+
         
     </div>
 
